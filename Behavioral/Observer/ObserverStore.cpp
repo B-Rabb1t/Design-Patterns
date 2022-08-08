4127811cd945b89189500078f856d79956451d2a
B-Rabb1t <georgeborsa@gmail.com>
2022-08-08 21:01:44 +0000

    No need for pointers

--- Behavioral/Observer/ObserverStore.cpp
+++ Behavioral/Observer/ObserverStore.cpp
@@ -7,7 +7,7 @@ using std::string;
 
 #define TOTAL_GROCERY_ITEMS 5
 
-struct GroceryItem 
+struct GroceryItem
 {
     string itemName;
     bool available;
@@ -17,7 +17,7 @@ struct GroceryItem
 std::vector<GroceryItem> groceryItems(TOTAL_GROCERY_ITEMS);
 
 // This is an interface class for Customer (Subscriber) class
-class Subscriber 
+class Subscriber
 {
 public:
     virtual ~Subscriber(){};
@@ -25,7 +25,7 @@ public:
 };
 
 // This is an interface class for GroceryStore (Publisher/Publisher) class
-class Publisher 
+class Publisher
 {
 public:
     virtual ~Publisher(){};
@@ -38,31 +38,31 @@ public:
  * GroceryStore is the Publisher/Publisher here that owns some important
  * state and notifies subscribers (i.e. customers) when the state changes.
  */
-class GroceryStore : public Publisher 
+class GroceryStore : public Publisher
 {
 private:
-    //elements in the Vector are stored in contiguous memory locations
-    //also: vector brings more fun
+    // elements in the Vector are stored in contiguous memory locations
+    // also: vector brings more fun
     std::vector<Subscriber *> subscribers;
-    
+
 public:
     GroceryStore(std::vector<GroceryItem> grocery)
     {
         groceryItems = grocery;
     }
-    
-    virtual ~GroceryStore() 
+
+    virtual ~GroceryStore()
     {
         cout << "Goodbye, I was the Publisher.\n";
     }
 
-    void subscribe(Subscriber *observer) 
+    void subscribe(Subscriber *observer)
     {
         cout << "An observer has subscribed\n";
         subscribers.push_back(observer);
     }
 
-    void unsubscribe(Subscriber *observer) 
+    void unsubscribe(Subscriber *observer)
     {
         cout << "A customer has unsubscribed\n";
         subscribers.erase(find(begin(subscribers), end(subscribers), observer));
@@ -70,7 +70,7 @@ public:
 
     void notify(string msg)
     {
-        for(auto subscriber : subscribers)
+        for (auto subscriber : subscribers)
         {
             cout << "GroceryStore is sending an update to the customer\n";
             subscriber->update(msg);
@@ -78,10 +78,11 @@ public:
     }
 
     void refillInventory(string itemName)
-    {        
-        auto refilled = find_if(begin(groceryItems), end(groceryItems), [&](GroceryItem item){return item.itemName == itemName;});
+    {
+        auto refilled = find_if(begin(groceryItems), end(groceryItems), [&](GroceryItem item)
+                                { return item.itemName == itemName; });
 
-        if(refilled != end(groceryItems))
+        if (refilled != end(groceryItems))
         {
             refilled->available = true;
             cout << "Refilling the stock for item " << itemName << "\n";
@@ -90,28 +91,28 @@ public:
     }
 };
 
-class Customer : public Subscriber 
+class Customer : public Subscriber
 {
 private:
     GroceryStore &subject_;
 
 public:
-    Customer(GroceryStore &subject) : subject_(subject) 
+    Customer(GroceryStore &subject) : subject_(subject)
     {
         this->subject_.subscribe(this);
     }
 
-    virtual ~Customer() 
+    virtual ~Customer()
     {
         cout << "Goodbye, I was the Subscriber.\n";
     }
 
-    void update(const string &msg) 
+    void update(const string &msg)
     {
         cout << "Hey customer! A new message is available --> " << msg << "\n";
     }
 
-    void unsubscribeCustomer() 
+    void unsubscribeCustomer()
     {
         cout << "Customer wants to unsubscribe the Grocery Store notifications - ";
         subject_.unsubscribe(this);
@@ -121,36 +122,35 @@ public:
     {
         cout << "Customer wants to purchase item " << itemName << " - ";
 
-        auto purchased = find_if(begin(groceryItems), end(groceryItems), [&](GroceryItem item){return item.itemName == itemName && item.available;});
+        auto purchased = find_if(begin(groceryItems), end(groceryItems), [&](GroceryItem item)
+                                 { return item.itemName == itemName && item.available; });
 
-        if(purchased != end(groceryItems))
+        if (purchased != end(groceryItems))
         {
             cout << "Item " << itemName << " is available\n";
             return true;
         }
 
-        cout << "The item " << itemName << " is not available in the Grocery Store\n"; 
+        cout << "The item " << itemName << " is not available in the Grocery Store\n";
         return false;
     }
 };
 
-int main() {
-    std::vector<GroceryItem> itemList = {{"Flour", true}, {"Rice", true}, {"Brown Bread", false}, \
-                              {"Blueberry Syrup", false}, {"Detergent", true}};   
-    GroceryStore *objGs = new GroceryStore(itemList);
-    Customer *objCust = new Customer(*objGs);
+int main()
+{
+    std::vector<GroceryItem> itemList = {{"Flour", true}, {"Rice", true}, {"Brown Bread", false}, {"Blueberry Syrup", false}, {"Detergent", true}};
+    GroceryStore objGs(itemList);
+    Customer objCust(objGs);
 
-    objCust->purchaseItem("Detergent");
-    objCust->purchaseItem("Flour");
-    objCust->purchaseItem("Rice");
-    objCust->purchaseItem("Brown Bread");
-    objCust->purchaseItem("Blueberry Syrup");
+    objCust.purchaseItem("Detergent");
+    objCust.purchaseItem("Flour");
+    objCust.purchaseItem("Rice");
+    objCust.purchaseItem("Brown Bread");
+    objCust.purchaseItem("Blueberry Syrup");
     cout << "\n";
-    objGs->refillInventory("Brown Bread");
-    objCust->purchaseItem("Brown Bread");
-    objCust->unsubscribeCustomer();
+    objGs.refillInventory("Brown Bread");
+    objCust.purchaseItem("Brown Bread");
+    objCust.unsubscribeCustomer();
     cout << "\n";
-    objGs->refillInventory("Blueberry Syrup");
-    
-    return 0;
+    objGs.refillInventory("Blueberry Syrup");
 }
